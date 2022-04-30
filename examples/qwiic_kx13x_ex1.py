7f2358bf5884a9e9c105b0e8c4e81dce137f7236
Chris Phillips RPi4-Twang <c.phillips@fnc.co.uk>
2022-04-30 22:16:01 +0100

    Working for KX134

--- examples/qwiic_kx13x_ex1.py
+++ examples/qwiic_kx13x_ex1.py
@@ -47,8 +47,8 @@ import sys
 def runExample():
 
     print("\nSparkFun KX13X Accelerometer Example 1\n")
-    # myKx = qwiic_kx13x.QwiicKX134() # If using the KX134 un-comment this line and replace other instances of "kx132" with "kx134"
-    myKx = qwiic_kx13x.QwiicKX132()
+    myKx = qwiic_kx13x.QwiicKX134() # If using the KX134 un-comment this line and replace other instances of "kx132" with "kx134"
+    # myKx = qwiic_kx13x.QwiicKX132()
 
     if myKx.connected == False:
             print("The Qwiic KX13X Accelerometer device isn't connected to the system. Please check your connection", \
@@ -66,9 +66,9 @@ def runExample():
     while True:
             
         myKx.get_accel_data()
-        print("X: {0}g Y: {1}g Z: {2}g".format(myKx.kx132_accel.x,
-                                               myKx.kx132_accel.y,
-                                               myKx.kx132_accel.z))
+        print("X: {0}g Y: {1}g Z: {2}g".format(myKx.kx134_accel.x,
+                                               myKx.kx134_accel.y,
+                                               myKx.kx134_accel.z))
         time.sleep(.02) #Set delay to 1/Output Data Rate which is by default 50Hz 1/50 = .02
 
 
--- examples/qwiic_kx13x_ex2.py
+++ examples/qwiic_kx13x_ex2.py
@@ -48,8 +48,8 @@ import RPi.GPIO as GPIO
 def runExample():
 
     print("\nSparkFun KX13X Accelerometer Example 2\n")
-    # myKx = qwiic_kx13x.QwiicKX134() # If using the KX134 un-comment this line and replace other instances of "kx132" with "kx134"
-    myKx = qwiic_kx13x.QwiicKX132()
+    myKx = qwiic_kx13x.QwiicKX134() # If using the KX134 un-comment this line and replace other instances of "kx132" with "kx134"
+    # myKx = qwiic_kx13x.QwiicKX132()
 
     if myKx.connected == False:
         print("The Qwiic KX13X Accelerometer device isn't connected to the system. Please check your connection", \
@@ -73,9 +73,9 @@ def runExample():
         if GPIO.input(dataReadyPin) == 1:
 
             myKx.get_accel_data()
-            print("X: {0}g Y: {1}g Z: {2}g".format(myKx.kx132_accel.x,
-                                                   myKx.kx132_accel.y,
-                                                   myKx.kx132_accel.z))
+            print("X: {0}g Y: {1}g Z: {2}g".format(myKx.kx134_accel.x,
+                                                   myKx.kx134_accel.y,
+                                                   myKx.kx134_accel.z))
 
         time.sleep(.02) #Set delay to 1/Output Data Rate which is by default 50Hz 1/50 = .02
 
